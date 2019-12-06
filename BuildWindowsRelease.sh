54bde851405739bee349b34a80670348e3a39bc8
Adrian Pop <adrian.pop@liu.se>
2019-12-06 09:56:20 +0200

    fix release directory, update text

--- BuildWindowsRelease.sh
+++ BuildWindowsRelease.sh
@@ -165,7 +165,7 @@ git log --name-status --graph --submodule > ${OMC_INSTALL_FILE_PREFIX}-ChangeLog
 
 # make the readme
 export DATESTR=`date +"%Y-%m-%d_%H-%M"`
-echo "Automatic build of OpenModelica by testwin.openmodelica.org at date: ${DATESTR} from revision: ${REVISION}" >> ${OMC_INSTALL_FILE_PREFIX}-README.txt
+echo "Automatic build of OpenModelica by *.openmodelica.org at date: ${DATESTR} from revision: ${REVISION}" >> ${OMC_INSTALL_FILE_PREFIX}-README.txt
 echo " " >> ${OMC_INSTALL_FILE_PREFIX}-README.txt
 echo "Read OpenModelica-${REVISION}-ChangeLog.txt for more info on changes." >> ${OMC_INSTALL_FILE_PREFIX}-README.txt
 echo " " >> ${OMC_INSTALL_FILE_PREFIX}-README.txt
@@ -212,9 +212,8 @@ echo "Contact us at OpenModelica@ida.liu.se for further issues or questions." >>
 #echo "Running testsuite trace"
 #make -f 'Makefile.omdev.mingw' ${MAKETHREADS} testlogwindows > tmpTime.log 2>&1
 
-echo "Check HUDSON testserver for the testsuite trace here (match revision ${REVISION} to build jobs): " >> ${OMC_INSTALL_FILE_PREFIX}-testsuite-trace.txt
-echo "  https://test.openmodelica.org/hudson/" >> ${OMC_INSTALL_FILE_PREFIX}-testsuite-trace.txt
-echo "  https://test.openmodelica.org/hudson/job/OM_Win/lastBuild/console" >> ${OMC_INSTALL_FILE_PREFIX}-testsuite-trace.txt
+echo "Check Jenkins testserver for the testsuite trace here (match revision ${REVISION} to build jobs): " >> ${OMC_INSTALL_FILE_PREFIX}-testsuite-trace.txt
+echo "  https://test.openmodelica.org/jenkins/" >> ${OMC_INSTALL_FILE_PREFIX}-testsuite-trace.txt
 #cat tmpTime.log >> ${OMC_INSTALL_FILE_PREFIX}-testsuite-trace.txt
 #rm -f tmpTime.log
 
@@ -224,16 +223,15 @@ cd ${OMC_INSTALL_PREFIX}
 # move the last nightly build to the older location
 ssh ${SSHUSER}@build.openmodelica.org <<ENDSSH
 #commands to run on remote host
-mkdir -p public_html/omc/builds/windows/releases/v1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/older
-cd public_html/omc/builds/windows/releases/v1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/
+mkdir -p public_html/omc/builds/windows/releases/1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/older
+cd public_html/omc/builds/windows/releases/1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/
 #rm -f older/*.* || true
 mv -f OpenModelica* older/ || true
 ENDSSH
-scp OpenModelica*${PLATFORM}* ${SSHUSER}@build.openmodelica.org:public_html/omc/builds/windows/releases/v1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/
+scp OpenModelica*${PLATFORM}* ${SSHUSER}@build.openmodelica.org:public_html/omc/builds/windows/releases/1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/
 ssh ${SSHUSER}@build.openmodelica.org <<ENDSSH
 #commands to run on remote host
-cd public_html/omc/builds/windows/releases/v1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/
-pwd
+cd public_html/omc/builds/windows/releases/1.15/maintenance/${OM_ENCRYPT}${PLATFORM}/
 pwd
 echo "ln -s OpenModelica-${REVISION}-${PLATFORM}.exe OpenModelica-latest.exe"
 ln -s OpenModelica-${REVISION}-${PLATFORM}.exe OpenModelica-latest.exe
