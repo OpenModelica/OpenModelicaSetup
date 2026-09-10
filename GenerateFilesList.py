4bbfa7245fc326a8e61560a7287a58031de0bc66
Adrian Pop <adrian.pop@liu.se>
2026-09-10 08:42:21 +0200

    fix the backslash quoting

--- GenerateFilesList.py
+++ GenerateFilesList.py
@@ -78,36 +78,36 @@ if __name__ == "__main__":
   base_directory = "..\\build\\bin"
   files_to_exclude = [r".*\.git"]
   list_files(base_directory, [], files_to_exclude, f, True)
-  f.write('${File} "..\OSMC-License.txt" "OSMC-License.txt"' + '\n')
+  f.write('${File} "..\\OSMC-License.txt" "OSMC-License.txt"' + '\n')
   # Copy the openssl binaries
   if args.PLATFORMVERSION == "32":
-    f.write('${File} "bin\\32bit\libeay32.dll" "libeay32.dll"' + '\n')
-    f.write('${File} "bin\\32bit\libssl32.dll" "libssl32.dll"' + '\n')
-    f.write('${File} "bin\\32bit\ssleay32.dll" "ssleay32.dll"' + '\n')
+    f.write('${File} "bin\\32bit\\libeay32.dll" "libeay32.dll"' + '\n')
+    f.write('${File} "bin\\32bit\\libssl32.dll" "libssl32.dll"' + '\n')
+    f.write('${File} "bin\\32bit\\ssleay32.dll" "ssleay32.dll"' + '\n')
   else:
-    f.write('${File} "bin\\64bit\libeay32.dll" "libeay32.dll"' + '\n')
-    f.write('${File} "bin\\64bit\libssl-1_1-x64.dll" "libssl-1_1-x64.dll"' + '\n')
-    f.write('${File} "bin\\64bit\ssleay32.dll" "ssleay32.dll"' + '\n')
+    f.write('${File} "bin\\64bit\\libeay32.dll" "libeay32.dll"' + '\n')
+    f.write('${File} "bin\\64bit\\libssl-1_1-x64.dll" "libssl-1_1-x64.dll"' + '\n')
+    f.write('${File} "bin\\64bit\\ssleay32.dll" "ssleay32.dll"' + '\n')
   # Create icons directory and copy files in it
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\icons"' + '\n')
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\icons"' + '\n')
   base_directory = "icons"
   files_to_exclude = [r".*\.git"]
   list_files(base_directory, [], files_to_exclude, f, True)
-  f.write('${File} "..\OMEdit\OMEditLIB\Resources\icons\omedit.ico" "omedit.ico"' + '\n')
-  f.write('${File} "..\OMOptim\OMOptim\GUI\Resources\omoptim.ico" "omoptim.ico"' + '\n')
-  f.write('${File} "..\OMPlot\OMPlot\OMPlotGUI\Resources\icons\omplot.ico" "omplot.ico"' + '\n')
-  f.write('${File} "..\OMShell\OMShell\OMShellGUI\Resources\omshell.ico" "omshell.ico"' + '\n')
-  f.write('${File} "..\OMNotebook\OMNotebook\OMNotebookGUI\Resources\OMNotebook_icon.ico" "OMNotebook_icon.ico"' + '\n')
+  f.write('${File} "..\\OMEdit\\OMEditLIB\\Resources\\icons\\omedit.ico" "omedit.ico"' + '\n')
+  f.write('${File} "..\\OMOptim\\OMOptim\\GUI\\Resources\\omoptim.ico" "omoptim.ico"' + '\n')
+  f.write('${File} "..\\OMPlot\\OMPlot\\OMPlotGUI\\Resources\\icons\\omplot.ico" "omplot.ico"' + '\n')
+  f.write('${File} "..\\OMShell\\OMShell\\OMShellGUI\\Resources\\omshell.ico" "omshell.ico"' + '\n')
+  f.write('${File} "..\\OMNotebook\\OMNotebook\\OMNotebookGUI\\Resources\\OMNotebook_icon.ico" "OMNotebook_icon.ico"' + '\n')
   # Create include\omc directory and copy files in it
-  f.write('${AddItem} "\\\\?\\$INSTDIR\include"' + '\n')
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\include\omc"' + '\n')
-  base_directory = "..\\build\include\omc"
+  f.write('${AddItem} "\\\\?\\$INSTDIR\\include"' + '\n')
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\include\\omc"' + '\n')
+  base_directory = "..\\build\\include\\omc"
   files_to_exclude = [r".*\.git"]
   list_files(base_directory, [], files_to_exclude, f, True)
   # Create lib\omc directory and copy files in it
-  f.write('${AddItem} "\\\\?\\$INSTDIR\lib"' + '\n')
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\lib\omc"' + '\n')
-  base_directory = "..\\build\lib\omc"
+  f.write('${AddItem} "\\\\?\\$INSTDIR\\lib"' + '\n')
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\lib\\omc"' + '\n')
+  base_directory = "..\\build\\lib\\omc"
   files_to_exclude = [r".*\.git"]
   list_files(base_directory, [], files_to_exclude, f, True)
   # Create tools directory and copy files in it
@@ -115,11 +115,11 @@ if __name__ == "__main__":
   # copy the setup file / readme
   OMDEV = os.environ['OMDEV']
   OMDEV = OMDEV.replace('/', '\\')
-  f.write('${File} "' + OMDEV + '\\tools\MSYS_SETUP.bat" "MSYS_SETUP.bat"' + '\n')
-  f.write('${File} "' + OMDEV + '\\tools\MSYS_SETUP.txt" "MSYS_SETUP.txt"' + '\n')
+  f.write('${File} "' + OMDEV + '\\tools\\MSYS_SETUP.bat" "MSYS_SETUP.bat"' + '\n')
+  f.write('${File} "' + OMDEV + '\\tools\\MSYS_SETUP.txt" "MSYS_SETUP.txt"' + '\n')
   # Create msys directory and copy files in it
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools\msys"' + '\n')
-  base_directory = OMDEV + "\\tools\msys"
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools\\msys"' + '\n')
+  base_directory = OMDEV + "\\tools\\msys"
   dirs_to_exclude = [r"tmp", r"qtcreator", r"Adwaita", r"OpenSceneGraph", r"gtk-doc" , r"poppler", r"man"
                      , r"python3.5", r"ActiveQt", r"Qt3DCore", r"Qt3DInput", r"Qt3DLogic", r"Qt3DQuick"
                      , r"Qt3DQuickInput" , r"Qt3DQuickRender", r"Qt3DRender", r"QtBluetooth", r"QtCLucene", r"QtConcurrent"
@@ -148,21 +148,21 @@ if __name__ == "__main__":
       dirs_to_exclude = dirs_to_exclude + [r"mingw32", r"ucrt64", r"clang64", r"clang32"]
   list_files(base_directory, dirs_to_exclude, files_to_exclude, f, True)
   # Create tmp folder for msys
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools\msys\\tmp"' + '\n')
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools\\msys\\tmp"' + '\n')
   # Create share directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share"' + '\n')
-  base_directory = "..\\build\share"
+  base_directory = "..\\build\\share"
   files_to_exclude = [r".*\.git"]
   list_files(base_directory, [], files_to_exclude, f, True)
   # Copy the OpenModelica web page & users guide url shortcut
-  f.write('${AddItem} "\\\\?\\$INSTDIR\share\doc"' + '\n')
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share\doc\omc"' + '\n')
-  f.write('${File} "..\doc\OpenModelica Project Online.url" "OpenModelica Project Online.url"' + '\n')
-  f.write('${File} "..\doc\OpenModelicaUsersGuide.url" "OpenModelicaUsersGuide.url"' + '\n')
+  f.write('${AddItem} "\\\\?\\$INSTDIR\\share\\doc"' + '\n')
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share\\doc\\omc"' + '\n')
+  f.write('${File} "..\\doc\\OpenModelica Project Online.url" "OpenModelica Project Online.url"' + '\n')
+  f.write('${File} "..\\doc\\OpenModelicaUsersGuide.url" "OpenModelicaUsersGuide.url"' + '\n')
   # Copy OMSens directory
-  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share\OMSens"' + '\n')
-  base_directory = "..\\build\share\OMSens"
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share\\OMSens"' + '\n')
+  base_directory = "..\\build\\share\\OMSens"
   files_to_exclude = [r".*\.git"]
   list_files(base_directory, [], files_to_exclude, f, True)
-  f.close
+  f.close()
   print("Successfully generated file FilesList.nsh")
